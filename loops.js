91ef540013981e40dc0191f4b722a650f8d9e036
MP-WD-2022 <Mrc.Ppls.Git@outlook.com>
2022-08-29 21:10:52 +0200

    loops-opdracht3

--- loops.js
+++ loops.js
@@ -18,13 +18,13 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
-for (let i=0 ; i<4; i++) {
-    console.log('loop...');
-}
-
-console.log('klaar!');
+// for (let i=0 ; i<4; i++) {
+//     console.log('loop...');
+// }
+//
+// console.log('klaar!');
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -39,7 +39,13 @@ console.log('klaar!');
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
 
-
+for (let i = 0; i < 16; i++) {
+    if ((i % 2) > 0) {
+        console.log([i] + ' is oneven');
+    } else {
+        console.log([i] + ' is even');
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
